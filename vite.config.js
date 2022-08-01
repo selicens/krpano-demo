8ad5f17232078492ce0a957bfc72a696bda5131b
ZhouWei <1244620067@qq.com>
2022-08-01 21:23:57 +0800

    update YML

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? './' : '/',
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
+  base: process.env.NODE_ENV === 'production' ? '/krpano-demo/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/krpano-demo/' : '/'
 })
