468e6a70596b30b1b78fabe4c3a5331b5a37fc13
ZhouWei <1244620067@qq.com>
2022-08-01 17:29:48 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/krpano-demo/',
-  publicPath: '/krpano-demo/'
+  base: process.env.NODE_ENV === 'production' ? './' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/krpano-demo/' : '/'
 })
